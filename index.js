e2d7790a005e19fd62e13363d2d36391d2444c79
Kevin Wither <kvn.wither@gmail.com>
2022-04-30 22:12:52 -0600

    commit tree 4d459d104b138756a1a3a9a13748798c4b84b340

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let message;
+
+  if (distance >=1 && distance <=400) {
+    return message = `This one is on me!`;
+  }else if (distance >=401 && distance <=2000) {
+    return message = `That will be twenty bucks.`;
+  }else if (distance >2000 && distance <=2500) {
+    return message = `I will gladly take your thirty bucks.`;
+  }else {
+    return message = `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  const message = (city === `NYC`) ? "Ok, sounds good." : "No go.";
+  return message;
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  switch (amount) {
+    case `generous`:
+      return `Thank you so much.`;
+    case 'not as generous':
+      return `Thank you.`;
+    default:
+      return `Bye.`;
+  }
 }
